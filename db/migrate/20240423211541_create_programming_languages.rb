d885c1cd7282c6ad6e7e216261d681c31fd70d08
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-04-23 17:16:53 -0400

    generate ProgrammingLanguage scaffold

---
+++ db/migrate/20240423211541_create_programming_languages.rb
@@ -0,0 +1,10 @@
+class CreateProgrammingLanguages < ActiveRecord::Migration[7.1]
+  def change
+    create_table :programming_languages do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
